dfea1b1b49edecb35ffa3cb295695894a43a8cfd
timtim40a <timtim40a@gmail.com>
2026-09-12 00:49:03 +0100

    Add win condition - pathfinding algorithm - better fool-proof checks - letters now corresponding to columns dynamically

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 const ERR_OCCUPIED: &str = "The requested square is already occupied by ";
 const ERR_BAD_INPUT: &str = "Bad input";
 
+
 #[derive(Debug)]
 #[derive(Clone)]
 #[derive(Copy)]
@@ -17,6 +18,14 @@ impl Colour {
         if colour == Colour::Black {Colour::White}
         else {Colour::Black}
     }
+}
+
+#[derive(PartialEq, Debug)]
+enum Compass {
+    North,
+    East,
+    South,
+    West,
 }
 
 struct Board {
@@ -46,18 +55,51 @@ impl Board {
         }
         println!("{}", "_".repeat(self.size * 8 + 1));
     }
-    
-    fn check_win(&self) {
-        println!("Checking win conditions");
+
+    fn check_win(&self, row: usize, file: usize, candidate: Colour, mut win_sum: Vec<Compass>, visited: &mut Vec<(usize, usize)>) -> Vec<Compass> {
+        if visited.contains(&(row, file)){
+            return win_sum;
+        } else {
+            visited.push((row, file))
+        }
+
+
+        if row == 0 {
+            win_sum.push(Compass::North);
+        }
+        if file == self.size - 1 {
+            win_sum.push(Compass::East);
+        }
+        if row == self.size - 1 {
+            win_sum.push(Compass::South);
+        }
+        if file == 0 {
+            win_sum.push(Compass::West);
+        }
+
+        if row > 0 && self.board[row-1][file] != None && self.board[row-1][file].unwrap() == candidate {
+            win_sum = self.check_win(row-1, file, candidate, win_sum, visited);
+        }
+        if file+1 < self.size && self.board[row][file+1] != None && self.board[row][file+1].unwrap() == candidate {
+            win_sum = self.check_win(row, file+1, candidate, win_sum, visited);
+        }
+        if row+1 < self.size && self.board[row+1][file] != None && self.board[row+1][file].unwrap() == candidate {
+            win_sum = self.check_win(row+1, file, candidate, win_sum, visited);
+        }
+        if file > 0 && self.board[row][file-1] != None && self.board[row][file-1].unwrap() == candidate {
+            win_sum = self.check_win(row, file-1, candidate, win_sum, visited);
+        }
+        win_sum
+
     }
 }
 
 fn main() {
-    let mut board: Board = Board::new(3);
-    gameloop(board)
+    let board: Board = Board::new(5);
+    game_loop(board)
 }
 
-fn gameloop(mut board: Board) {
+fn game_loop(mut board: Board) {
     let mut counter: Colour = Colour::Black;
     loop{
         board.print();
@@ -66,6 +108,7 @@ fn gameloop(mut board: Board) {
         io::stdin().read_line(&mut player_move).expect("TODO: panic message");
         let player_move = player_move.trim();
         let board_file: usize;
+        let board_row: usize;
         match &player_move {
             &"" => {
                 println!("{}", ERR_BAD_INPUT);
@@ -78,15 +121,13 @@ fn gameloop(mut board: Board) {
             },
             _ => {},
         }
-        match &player_move[0..1] {
-            "a" => {
-                board_file = 0
-            }
-            "b" => {
-                board_file = 1
-            }
-            "c" => {
-                board_file = 2
+        if !(player_move.len() == 2 && player_move.as_bytes()[0].is_ascii_alphabetic()) {
+            println!("{}", ERR_BAD_INPUT);
+            continue
+        }
+        match player_move.as_bytes()[0] {
+            b if b >= b'a' && ((b - b'a') as usize) < board.size => {
+                board_file = (b - b'a') as usize;
             }
             _ => {
                 println!("{}", ERR_BAD_INPUT);
@@ -94,30 +135,30 @@ fn gameloop(mut board: Board) {
             }
         }
         match &player_move[1..2] {
-            "1" => {
-                if board.board[0][board_file] == None { board.board[0][board_file] = Some(counter)}
-                else {
-                    println!("{}{:?}", ERR_OCCUPIED, board.board[0][board_file]);
-                    continue
-                }
-            }
-            "2" => {
-                if board.board[1][board_file] == None { board.board[1][board_file] = Some(counter)}
-                else {
-                    println!("{}{:?}", ERR_OCCUPIED, board.board[1][board_file]);
-                    continue
-                }
-            }
-            "3" => {
-                if board.board[2][board_file] == None { board.board[2][board_file] = Some(counter)}
-                else {println!("{}{:?}", ERR_OCCUPIED, board.board[2][board_file]); continue}
+            s if s.parse::<usize>().is_ok_and(|n| n <= board.size && n > 0) => {
+                board_row = s.parse::<usize>().unwrap() - 1;
             }
             _ => {
                 println!("{}", ERR_BAD_INPUT);
                 continue
             }
         }
-        board.check_win();
+        if board.board[board_row][board_file] == None { board.board[board_row][board_file] = Some(counter)}
+        else {
+            println!("{}{:?}", ERR_OCCUPIED, board.board[board_row][board_file]);
+            continue
+        }
+        let winner = board.check_win(board_row, board_file, counter, Vec::new(), &mut Vec::new());
+        if winner.contains(&Compass::North) && winner.contains(&Compass::South) {
+            println!("{:?} won by connecting North and South!", counter);
+            io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
+            return;
+        }
+        else if winner.contains(&Compass::East) && winner.contains(&Compass::West) {
+            println!("{:?} won by connecting East and West!", counter);
+            io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
+            return;
+        }
         counter = Colour::advance_turn(counter);
 
     }
