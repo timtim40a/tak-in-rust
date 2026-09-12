9bd7c1b1fbdce72eac30be2c96cc53289e9f1ef3
timtim40a <timtim40a@gmail.com>
2026-09-12 22:31:59 +0100

    Fix panics regarding non ASCII chars in input - update Stack definition - remove None checks from check_win

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,31 @@ impl Colour {
     }
 }
 
+#[derive(Copy, Debug, Clone)]
+#[derive(PartialEq, Eq)]
+enum StoneType {
+    Road(Colour),
+    Wall(Colour),
+}
+
+struct Stack {
+    contents:Vec<StoneType>,
+    max_height:u8
+}
+
+impl Stack {
+    fn get_top(&self) -> Option<&StoneType> {
+        self.contents.last()
+    }
+
+    fn new(max_height: u8) -> Stack {
+        Stack {
+            contents: Vec::new(),
+            max_height
+        }
+    }
+}
+
 #[derive(PartialEq, Debug)]
 enum Compass {
     North,
@@ -29,7 +54,7 @@ enum Compass {
 }
 
 struct Board {
-    board: Vec<Vec<Option<Colour>>>,
+    board: Vec<Vec<Option<StoneType>>>,
     size: usize,
 }
 
@@ -43,20 +68,23 @@ impl Board {
 
     fn print(&self) {
         for i in self.board.as_slice() {
-            println!("{}", "_".repeat(self.size * 8 + 1));
+            println!("{}", "_".repeat(self.size * 4 + 1));
             for j in i {
                 match j {
-                    Some(Colour::Black) => print!("| Black "),
-                    Some(Colour::White) => print!("| White "),
-                    None => print!("|       ")
+                    Some(StoneType::Road(Colour::Black)) => print!("| ░ "),
+                    Some(StoneType::Road(Colour::White)) => print!("| █ "),
+                    Some(StoneType::Wall(Colour::Black)) => print!("| ⧅ "), //║
+                    Some(StoneType::Wall(Colour::White)) => print!("| ⬔ "), //┃
+                    None => print!("|   ")
                 }
             }
             println!("|")
         }
-        println!("{}", "_".repeat(self.size * 8 + 1));
+        println!("{}", "_".repeat(self.size * 4 + 1));
     }
 
     fn check_win(&self, row: usize, file: usize, candidate: Colour, mut win_sum: Vec<Compass>, visited: &mut Vec<(usize, usize)>) -> Vec<Compass> {
+
         if visited.contains(&(row, file)){
             return win_sum;
         } else {
@@ -77,16 +105,16 @@ impl Board {
             win_sum.push(Compass::West);
         }
 
-        if row > 0 && self.board[row-1][file] != None && self.board[row-1][file].unwrap() == candidate {
+        if row > 0 && self.board[row-1][file] == Some(StoneType::Road(candidate)) {
             win_sum = self.check_win(row-1, file, candidate, win_sum, visited);
         }
-        if file+1 < self.size && self.board[row][file+1] != None && self.board[row][file+1].unwrap() == candidate {
+        if file+1 < self.size && self.board[row][file+1] == Some(StoneType::Road(candidate)) {
             win_sum = self.check_win(row, file+1, candidate, win_sum, visited);
         }
-        if row+1 < self.size && self.board[row+1][file] != None && self.board[row+1][file].unwrap() == candidate {
+        if row+1 < self.size && self.board[row+1][file] == Some(StoneType::Road(candidate)) {
             win_sum = self.check_win(row+1, file, candidate, win_sum, visited);
         }
-        if file > 0 && self.board[row][file-1] != None && self.board[row][file-1].unwrap() == candidate {
+        if file > 0 && self.board[row][file-1] == Some(StoneType::Road(candidate)) {
             win_sum = self.check_win(row, file-1, candidate, win_sum, visited);
         }
         win_sum
@@ -109,6 +137,11 @@ fn game_loop(mut board: Board) {
         let player_move = player_move.trim();
         let board_file: usize;
         let board_row: usize;
+        let stone_type: StoneType;
+        if !player_move.is_ascii() {
+            println!("{}", ERR_BAD_INPUT);
+            continue
+        }
         match &player_move {
             &"" => {
                 println!("{}", ERR_BAD_INPUT);
@@ -121,7 +154,7 @@ fn game_loop(mut board: Board) {
             },
             _ => {},
         }
-        if !(player_move.len() == 2 && player_move.as_bytes()[0].is_ascii_alphabetic()) {
+        if !(player_move.len() == 3 && player_move.as_bytes()[0].is_ascii_alphabetic()) {
             println!("{}", ERR_BAD_INPUT);
             continue
         }
@@ -143,21 +176,30 @@ fn game_loop(mut board: Board) {
                 continue
             }
         }
-        if board.board[board_row][board_file] == None { board.board[board_row][board_file] = Some(counter)}
+        match &player_move[2..3] {
+            "r" => stone_type = StoneType::Road(counter),
+            "w" => stone_type = StoneType::Wall(counter),
+            _ => {
+                println!("{}", ERR_BAD_INPUT);
+                continue
+            }
+        }
+        if board.board[board_row][board_file] == None { board.board[board_row][board_file] = Some(stone_type)}
         else {
             println!("{}{:?}", ERR_OCCUPIED, board.board[board_row][board_file]);
             continue
         }
-        let winner = board.check_win(board_row, board_file, counter, Vec::new(), &mut Vec::new());
-        if winner.contains(&Compass::North) && winner.contains(&Compass::South) {
-            println!("{:?} won by connecting North and South!", counter);
-            io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
-            return;
-        }
-        else if winner.contains(&Compass::East) && winner.contains(&Compass::West) {
-            println!("{:?} won by connecting East and West!", counter);
-            io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
-            return;
+        if stone_type == StoneType::Road(counter) {
+            let winner = board.check_win(board_row, board_file, counter, Vec::new(), &mut Vec::new());
+            if winner.contains(&Compass::North) && winner.contains(&Compass::South) {
+                println!("{:?} won by connecting North and South!", counter);
+                io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
+                return;
+            } else if winner.contains(&Compass::East) && winner.contains(&Compass::West) {
+                println!("{:?} won by connecting East and West!", counter);
+                io::stdin().read_line(&mut String::new()).expect("TODO: panic message");
+                return;
+            }
         }
         counter = Colour::advance_turn(counter);
 
